4b6d156200b0e9a5649537d953ffb5c8cf76a4b8
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-10-18 10:57:48 +0900

    feat : 디자인 세팅

--- app/src/main/java/com/team2127/meetu/HiltApplication.kt
+++ app/src/main/java/com/team2127/meetu/HiltApplication.kt
@@ -1,8 +1,16 @@
 package com.team2127.meetu
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class HiltApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Logger.addLogAdapter(AndroidLogAdapter())
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
