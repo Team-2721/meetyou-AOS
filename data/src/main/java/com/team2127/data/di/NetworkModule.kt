fe89b27e6bf333c256a96dc44f19641abd109d53
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-10-22 20:03:14 +0900

    FEAT : login session 유지하기
    + 비밀번호 입력시 *로 입력 기능 추가

--- data/src/main/java/com/team2127/data/di/NetworkModule.kt
+++ data/src/main/java/com/team2127/data/di/NetworkModule.kt
@@ -1,17 +1,19 @@
 package com.team2127.data.di
 
-import android.os.Build
 import com.squareup.moshi.Moshi
 import com.team2127.data.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttp
+import okhttp3.Interceptor
+import okhttp3.JavaNetCookieJar
 import okhttp3.OkHttpClient
+import okhttp3.internal.addHeaderLenient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.net.CookieManager
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
@@ -20,6 +22,10 @@ private const val BASE_URL = "http://meetyou-backend-prod.ap-northeast-2.elastic
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class NormaNetworkObject
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class SessionNetworkObject
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -53,6 +59,49 @@ object NetworkModule{
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
+
+    @SessionNetworkObject
+    @Singleton
+    @Provides
+    fun provideSessionOkHttpClient(
+        authInterceptor: Interceptor
+    ): OkHttpClient =
+        if (BuildConfig.DEBUG) {
+            val loggingInterceptor = HttpLoggingInterceptor().apply {
+                setLevel(HttpLoggingInterceptor.Level.BODY)
+            }
+            OkHttpClient.Builder()
+                .addInterceptor(authInterceptor)
+                .addInterceptor(loggingInterceptor)
+                .cookieJar(JavaNetCookieJar(CookieManager()))
+                .build()
+        } else {
+            OkHttpClient.Builder()
+                .addInterceptor(authInterceptor)
+                .cookieJar(JavaNetCookieJar(CookieManager()))
+                .build()
+        }
+
+    @SessionNetworkObject
+    @Singleton
+    @Provides
+    fun provideAuthRetrofit(
+        @SessionNetworkObject okHttpClient: OkHttpClient,
+        moshi: Moshi
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+//    {
+//        return Retrofit.Builder()
+//            .client(okHttpClient)
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(MoshiConverterFactory.create(moshi))
+//            .build()
+//    }
 
     @Singleton
     @Provides
---
+++ data/src/main/java/com/team2127/data/intercepter/SessionInterceptor.kt
@@ -0,0 +1,12 @@
+package com.team2127.data.intercepter
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class SessionInterceptor(
+
+): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        TODO("Not yet implemented")
+    }
+}
